9bbd9ac5feece91d86b30ec1d8700f6739e781e3
yassCan <89553163+yassCan@users.noreply.github.com>
2026-06-10 13:42:57 +0200

    Remove CUBE class and associated methods
    Removed the CUBE class and its related methods.

--- vertex.js
+++ vertex.js
@@ -21,8 +21,6 @@ class Vertex {
         const t = (a * x + b * y + c * z + d) / denom;
 
         // Projected point
-        
-
         return  {
             x: (x - a * t),
             y: y - b * t,
@@ -30,6 +28,25 @@ class Vertex {
         }; 
 
 
+    }
+
+    perspectiveProjection(camera) {
+        const ray = (subVector(this.pos, camera.position))
+        const {x,y,z} = ray
+        const {a,b,c,d} = camera.coeff
+        const {x: xv, y: yv, z: zv} = this.pos
+        const {
+            x: cx,
+            y: cy,
+            z: cz
+        } = camera.position
+       
+        const t = (d + a*xv + b*yv + c*zv) / (a*x + b*y + c*z)
+        console.log(t);
+        
+        return {
+            Px: cx+camera.direction.x + x*t, Py: cy+camera.direction.y + y*t, Pz: cz+camera.direction.y + z*t
+        }
     }
 
     rotateX(theta) {
@@ -62,99 +79,5 @@ class Vertex {
 }
 
 
-
-
-class CUBE {
-    constructor(x,y,z,side){
-        this.pos = {x,y,z}
-        this.side = side/((z == 0) ? 1 : z)
-        this.verteces = [
-
-            // Front face
-            new Vertex(x-side/2, y-side/2,  z),
-            new Vertex(x+side/2, y-side/2,  z),
-            new Vertex(x-side/2,  y+side/2,  z),
-            new Vertex(x+side/2,  y+side/2,  z),
-
-            // Back face
-            new Vertex(x-side/2, y-side/2,  z+side),
-            new Vertex(x+side/2, y-side/2,  z+side),
-            new Vertex(x-side/2,  y+side/2,  z+side),
-            new Vertex(x+side/2,  y+side/2,  z+side),
-
-        ];
-
-    }
-
-    rotateY(theta) {
-        for(const vertex of this.verteces) {
-            
-            const v = {
-                x: vertex.pos.x - this.pos.x,
-                y: vertex.pos.y - this.pos.y,
-                z: vertex.pos.z - this.pos.z
-            }
-            // console.log(v);
-            vertex.rotateY(theta)
-            
-            // vertex.pos.x += this.pos.x
-            // vertex.pos.y += this.pos.y
-            // vertex.pos.z += this.pos.z
-        }
-    }
-
-    rotateX(theta) {
-        for(const vertex of this.verteces) {
-            
-            const v = {
-                x: vertex.pos.x - this.pos.x,
-                y: vertex.pos.y - this.pos.y,
-                z: vertex.pos.z - this.pos.z
-            }
-
-            vertex.rotateX(theta)
-            // console.log(v);
-            
-            // vertex.rotateY(theta)
-            // vertex.pos.x += this.pos.x
-            // vertex.pos.y += this.pos.y
-            // vertex.pos.z += this.pos.z
-        }
-    }
-
-  
-
-    render() {
-
-            
-        const p0 = this.verteces[0].projectInto(camera)
-        const p1 = this.verteces[1].projectInto(camera)
-        const p2 = this.verteces[2].projectInto(camera)
-        const p3 = this.verteces[3].projectInto(camera)
-
-        const p4 = this.verteces[4].projectInto(camera)
-        const p5 = this.verteces[5].projectInto(camera)
-        const p6 = this.verteces[6].projectInto(camera)
-        const p7 = this.verteces[7].projectInto(camera)
-
-        // Front
-        line(ctx, camera.position.x - p0.x, camera.position.y - p0.y, camera.position.x - p1.x, camera.position.y - p1.y, 'lime')
-        line(ctx, camera.position.x - p1.x, camera.position.y - p1.y, camera.position.x - p3.x, camera.position.y - p3.y, 'lime')
-        line(ctx, camera.position.x - p3.x, camera.position.y - p3.y, camera.position.x - p2.x, camera.position.y - p2.y, 'lime')
-        line(ctx, camera.position.x - p2.x, camera.position.y - p2.y, camera.position.x - p0.x, camera.position.y - p0.y, 'lime')
-
-        // Back
-        line(ctx, camera.position.x - p4.x, camera.position.y - p4.y, camera.position.x - p5.x, camera.position.y - p5.y, 'lime')
-        line(ctx, camera.position.x - p5.x, camera.position.y - p5.y, camera.position.x - p7.x, camera.position.y - p7.y, 'lime')
-        line(ctx, camera.position.x - p7.x, camera.position.y - p7.y, camera.position.x - p6.x, camera.position.y - p6.y, 'lime')
-        line(ctx, camera.position.x - p6.x, camera.position.y - p6.y, camera.position.x - p4.x, camera.position.y - p4.y, 'lime')
-
-        // Connections
-        line(ctx, camera.position.x - p0.x, camera.position.y - p0.y, camera.position.x - p4.x, camera.position.y - p4.y, 'lime')
-        line(ctx, camera.position.x - p1.x, camera.position.y - p1.y, camera.position.x - p5.x, camera.position.y - p5.y, 'lime')
-        line(ctx, camera.position.x - p2.x, camera.position.y - p2.y, camera.position.x - p6.x, camera.position.y - p6.y, 'lime')
-        line(ctx, camera.position.x - p3.x, camera.position.y - p3.y, camera.position.x - p7.x, camera.position.y - p7.y, 'lime')
-    }
-}
 
 
